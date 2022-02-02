a1097a76f894b28103af9276572e17a9431c75dd
mystrycoding2009 <91905331+mystrycoding2009@users.noreply.github.com>
2022-02-02 13:39:04 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,100 @@
+// how to write in main content of
+// document.write("hello whats up")
+
+// Console. Api
+// console.log("0")
+// console.warn("hello my name is warning")
+// console.error("this is an error")
+
+// Java script variables
+//  var
+
+// var number1 = 120;
+// var number2 = 140;
+// console.log(number1 + number2);
+// console.log(number1, number2);
+
+// Datatypes in javascript
+// 1. Primitive datatypes - strings,number,object,booleans, undefined, Null, Symbol
+
+// strings
+// var str = "This is a cool string";
+// console.log(str);
+
+// Numbers
+
+// var num = 45;
+// var num2 = 12;
+// console.log(num - num2);
+
+// object (primitive)
+
+// var salary = {
+//     Rohan: 12000,
+//     Shubh: 30000,
+//     Abhay: 80000,
+//     Raghav: 100000000,
+// }
+
+// console.log(salary);
+
+//booleans
+// var a = true;
+// var b = false;
+
+// console.log(a,b)
+
+// undefined
+// var und = undefined;
+// console.log(und);
+
+// //Null
+
+// var cwa = null;
+// console.log(cwa)
+
+// 2. Reference datatypes = total 2 types
+// Arrays
+            // javascript me 0 se start hote hai
+var arr = [1,2,3,"Abhay",5,6,7]
+// console.log(arr)
+
+//Operators in javascript are
+
+//Arthmetic operators
+// var a = 45;
+// var b = 100;
+// console.log("the sum of a and b is", a+b)
+// console.log("the minus of a and b is", a-b)
+// console.log("the multiplication of a and b is", a*b)
+// console.log("the division of a and b is", a/b)
+
+//Assingment operators
+// var b = 100;
+// var c =b
+// c += 2
+// console.log(c)
+
+// Comparsion operators for
+
+// var x = 34;
+// var y = 100;
+// console.log(34 == 100)
+// console.log(34 >= 100)
+// console.log(34 <= 100)
+
+//logical operators
+
+// and
+ 
+// console.log(true && true)
+// console.log(true && false)
+
+// or
+
+// console.log(true || true)
+// console.log(true  || false)
+
+// sach ko jhoot banale fuctions
+// console.log(!true) // false print
+// console.log(!false) // true print
